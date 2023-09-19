591b64a537c8d7a26be73d17a4da7b338bcfdf46
chuong38 <chuongkhongtich@gmail.com>
2023-09-19 20:58:24 +0700

    up

--- src/components/ConfirmComponent.jsx
+++ src/components/ConfirmComponent.jsx
@@ -8,7 +8,7 @@ const ConfirmComponent = () => {
 
     const [activePopup, setActivePopup] = useState(false);
     const [activeLink, setActiveLink] = useState(false);
-    const [timeLeft, setTimeLeft] = useState(20); 
+    const [timeLeft, setTimeLeft] = useState(300); 
 
     const [activeWaring, setActiveWaring] = useState(false);
     const [firstCode, setFirstCode] = useState();
